f1a5353d7417e5ff8254b92b6ccc09fa58eb1d1b
Smalch <Artems98@mail.ru>
2016-11-18 17:25:06 +0300

    Mne len pereklychatsa na russki

---
+++ src/Hand.java
@@ -0,0 +1,7 @@
+import java.util.LinkedList;
+
+/**
+ * Created by student1 on 14.11.16.
+ */
+public class Hand extends LinkedList<Card> {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,36 @@
+import Command.Command;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by student1 on 11.11.16.
  */
 public class Main {
 
     public static void main(String[] args) {
-        for (Suit s : Suit.values())
-            for (Value v : Value.values()) {
-                Card c = new Card(s,v);
-                System.out.println(c);
+        List<Player> players = new LinkedList<>();
+        players.add(new Computer());
+        players.add(new Computer());
+        players.add(new Human());
+        Dealer dealer = new Dealer();
+        players.add(dealer);
+
+        for(Player player: players){
+            dealer.deal(player);
+            dealer.deal(player);
+            System.out.println(player.hand);
+        }
+
+
+        for(Player player: players){
+            while(true){
+                Command command = player.decision(); //decision-решение
+                if(command==Command.STAND)
+                    break;
+                if (command ==Command.HIT)
+                    dealer.deal(player);
             }
+        }
     }
 }
